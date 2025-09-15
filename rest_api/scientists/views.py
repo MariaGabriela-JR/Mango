e5a9036303a3970970f4bd6c20fd4228d05e2035
Dalla <matheusdallaths@gmail.com>
2025-09-15 01:39:41 -0300

    fix: ajuste de autenticação dos cientistas

--- rest_api/scientists/views.py
+++ rest_api/scientists/views.py
@@ -6,8 +6,6 @@ from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 from .models import Scientist
 from .serializers import ScientistSerializer
 from django.utils.crypto import get_random_string
-from django.conf import settings
-from django.core.mail import send_mail
 
 class ScientistCreateView(generics.CreateAPIView):
     queryset = Scientist.objects.all()
@@ -35,45 +33,24 @@ class ScientistCreateView(generics.CreateAPIView):
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         scientist = serializer.save(
-            is_verified = False,
-            verification_token = verification_token
+            is_verified=False,
+            verification_token=verification_token
         )
-
-        self.send_approval_email(scientist, verification_token)
 
         response_data = serializer.data
         response_data.pop('password', None)
-        response_data['message'] = 'Conta criada. Aguarde verificação administrativa.'
+        response_data['message'] = 'Conta criada com sucesso.'
+        response_data['verification_token'] = verification_token
 
         return Response(response_data, status=status.HTTP_201_CREATED)
-    
-    def send_approval_email(self, scientist, token):
-        subject = 'Nova conta de cientista precisa de verificação'
-        message = f'''
-        Nova conta de cientista criada:
 
-        Nome: {scientist.first_name} {scientist.last_name}
-        Email: {scientist.email}
-        Instituição: {scientist.institution}
-        Especialização: {scientist.specialization}
-
-        Link para aprovar: {settings.FRONTEND_URL}/admin/verify-scientist/{token}/
-        '''
-        send_mail(
-            subject,
-            message,
-            settings.DEFAULT_FROM_EMAIL,
-            [settings.ADMIN_EMAIL],
-            fail_silently=False,
-        )
-    
 class ScientistProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = ScientistSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
-    
+
 @api_view(['POST'])
 @permission_classes([permissions.IsAdminUser])
 def verify_scientists(request, token):
@@ -82,13 +59,5 @@ def verify_scientists(request, token):
     scientist.is_verified = True
     scientist.verification_token = None
     scientist.save()
-
-    send_mail(
-        'Sua conta foi aprovada!',
-        'Sua conta de cientista foi verificada e aprovada!',
-        settings.DEFAULT_FROM_EMAIL,
-        [scientist.email],
-        fail_silently=False,
-    )
 
     return Response({'status': 'Cientista verificado com sucesso.'})
