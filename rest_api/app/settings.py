fd7eecb5134238122bff33a4f041f02788416278
Matheus Dalla <matheusdallaths@gmail.com>
2025-12-03 12:02:26 -0300

    fix: pequena correção envolvendo e-mails

--- rest_api/app/settings.py
+++ rest_api/app/settings.py
@@ -182,14 +182,6 @@ AUTHENTICATION_BACKENDS = [
     "authentication.backends.ScientistBackend",
 ]
 
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
-ADMIN_EMAIL = config('ADMIN_EMAIL', default='')
-
 FASTAPI_DB_CONFIG = {
     'HOST': os.getenv('FASTAPI_DB_USER', 'zeim'),
     'PORT': os.getenv('FASTAPI_DB_PORT', '5440'),
